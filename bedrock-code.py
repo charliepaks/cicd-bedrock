c087f550c1ca9ac5f8c546e91faaed5fcca8ba86
Charles Chibueze <charlesukpaka@ymail.com>
2024-12-10 15:52:20 +0100

    more changes

--- bedrock-code.py
+++ bedrock-code.py
@@ -5,7 +5,7 @@ import os
 import sys
 import re
 
-# Retrieve AWS credentials and region from environment variables
+
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
